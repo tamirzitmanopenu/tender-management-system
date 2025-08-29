a9a99cde36684862c62bb74b875a673751cf2eeb
Tamir Zitman <zitamir4@telem.openu.ac.il>
2025-08-29 21:00:50 +0300

    fix dotenv

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,9 @@ from app import create_app
 application = create_app()
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
     # dev run
     application.run(host="0.0.0.0", port=5000, debug=True)
