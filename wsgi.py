bd3ce27055fd0481741eb9f0a876cf7e7264eb1a
Tamir Zitman <zitamir4@telem.openu.ac.il>
2025-08-29 21:26:26 +0300

    Merge pull request #19
    * load_dotenv globally once

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,10 @@
 from app import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 application = create_app()
 
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
     # dev run
     application.run(host="0.0.0.0", port=5000, debug=True)
